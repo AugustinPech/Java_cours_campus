17187707a2c9ba3e0592ecce9a582c2f94e83db2
AugustinPech <80385451+AugustinPech@users.noreply.github.com>
2024-05-21 08:53:51 +0200

    au secours

---
+++ DonjonAndDragons2/test/java/com/DungeonAndDragons2/PlayableTests.java
@@ -0,0 +1,43 @@
+package DonjonAndDragons2.test.java.com.DungeonAndDragons2;
+
+import DonjonAndDragons2.src.models.Game.Menu;
+import DonjonAndDragons2.src.models.Game.utilities.status.Status;
+
+import org.junit.jupiter.api.Test;
+
+import DonjonAndDragons2.src.models.Game.utilities.Stats;
+import DonjonAndDragons2.src.models.Caracters.Caracter;
+import DonjonAndDragons2.src.models.Caracters.NPC.Gobelin;
+import DonjonAndDragons2.src.models.Caracters.Player.Warrior;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.util.ArrayList;
+
+class PlayableTests {
+    @Test
+    void testApplyStatus(){
+        Caracter caracter = new Gobelin(2);
+        ArrayList<Status> statuses = new ArrayList<>();
+        Stats stats = new Stats(100, 0, 5, 5, 5, 5, 0);
+        
+        statuses.add(new Status(stats, 1, "poison"));
+        statuses.add(new Status(stats, 0, "poison"));
+        statuses.add(new Status(stats, 2, "poison"));
+
+        caracter. setStatuses(statuses);
+        for (Status status : statuses) {
+            caracter.applyStatuses();
+        }
+        assertEquals(1, caracter.getStatuses().size());
+    }
+    @Test
+    void testAddStatus(){
+        Caracter caracter = new Gobelin(2);
+        caracter.addStatus(new Status(new Stats(100, 0, 5, 5, 5, 5, 0), 1, "poison"));
+
+        assertEquals(1, caracter.getStatuses().size());
+    }
+}
--- DonjonAndDragons2/test/java/com/DungeonAndDragons2/StatsTests.java
+++ DonjonAndDragons2/test/java/com/DungeonAndDragons2/StatsTests.java
@@ -12,7 +12,6 @@ import static org.junit.jupiter.api.Assertions.*;
 
 
 class StatsTests {
-
     @Test
     void testMerge() {
         try {
